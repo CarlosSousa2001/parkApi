57930f56b09a2e1f9e6e1a04d34da8607671cd0b
carlos <carlosramos10k@gmail.com>
2023-08-18 15:23:45 -0300

    criado entity usuario

---
+++ src/main/java/com/sistemacar/parkapi/entity/Usuario.java
@@ -0,0 +1,128 @@
+package com.sistemacar.parkapi.entity;
+
+import com.sistemacar.parkapi.enums.Role;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(name = "username", nullable = false,unique = true,length = 100)
+    private String username;
+    @Column(name = "username", nullable = false,length = 255)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @Column(name = "modificado_por")
+    private String modificadorPor;
+
+    public Usuario(){}
+
+    public Usuario(UUID id, String username, String password, Role role, LocalDateTime dataCriacao, LocalDateTime dataModificacao, String criadoPor, String modificadorPor) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.dataCriacao = dataCriacao;
+        this.dataModificacao = dataModificacao;
+        this.criadoPor = criadoPor;
+        this.modificadorPor = modificadorPor;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(LocalDateTime dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
+
+    public String getCriadoPor() {
+        return criadoPor;
+    }
+
+    public void setCriadoPor(String criadoPor) {
+        this.criadoPor = criadoPor;
+    }
+
+    public String getModificadorPor() {
+        return modificadorPor;
+    }
+
+    public void setModificadorPor(String modificadorPor) {
+        this.modificadorPor = modificadorPor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

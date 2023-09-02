3fa754f97fa044dd875d8872ad9f3b8e9fbdab65
carlos <carlosramos10k@gmail.com>
2023-09-02 17:25:06 -0300

    erro nos teste resolvidos

--- src/test/java/com/sistemacar/parkapi/UsuarioIT.java
+++ src/test/java/com/sistemacar/parkapi/UsuarioIT.java
@@ -1,20 +1,80 @@
 package com.sistemacar.parkapi;
 
+import com.sistemacar.parkapi.DTO.UsuarioCreateDTO;
+import com.sistemacar.parkapi.DTO.UsuarioResponseDTO;
+import com.sistemacar.parkapi.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UsuarioIT {
 
-    @Autowired
+    @Autowired(required = true)
     WebTestClient testClient;
 
     @Test
-    public void 
+    public void createUsuario_ConUsernamePasswordValidos_RetornarUsuarioCriadocomStatus201(){
+       UsuarioResponseDTO responseBody = testClient
+                .post()
+                .uri("/usuarios/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tody@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UsuarioResponseDTO.class)
+                .returnResult().getResponseBody();
+
+       org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+       org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+       org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@gmail.com");
+       org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENT");
+    }
+    @Test
+    public void createUsuario_ConUsernameInvalidos_RetornarErrorMenssageStatus422(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/usuarios/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/usuarios/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tody@", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/usuarios/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tody@gmail", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
 
 }
--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,6 +1,4 @@
-INSERT INTO USUARIOS(id, name, password, role)
-    values("30a8861f-bf0a-4e3b-8dce-31757a5d5e67","angel@gmail.com", "123456", "ROLE_ADMIN");
-INSERT INTO USUARIOS(id, name, password, role)
-    values("c596b5b5-c656-4e6e-8f68-93b43bc1ad4e","ANA@gmail.com", "123456", "ROLE_USER");
-INSERT INTO USUARIOS(id, name, password, role)
-    values("b9b459fb-7b70-4275-a130-7c6c7fe5f854","julia@gmail.com", "123456", "ROLE_USER");
+INSERT INTO usuarios(id, username, password, role) VALUES (1, 'angel@gmail.com', '123456', 'ROLE_ADMIN');
+INSERT INTO usuarios(id, username, password, role) VALUES (2, 'ANA@gmail.com', '123456', 'ROLE_ADMIN');
+INSERT INTO usuarios(id, username, password, role) VALUES (3, 'julia@gmail.com', '123456', 'ROLE_ADMIN');
+

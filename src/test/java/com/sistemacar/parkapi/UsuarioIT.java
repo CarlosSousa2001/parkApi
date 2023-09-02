8565ca6b3faaff7ee4b8c18aa23be67b90c2105b
carlos <carlosramos10k@gmail.com>
2023-09-02 17:31:19 -0300

    teste usuario repetido

--- src/test/java/com/sistemacar/parkapi/UsuarioIT.java
+++ src/test/java/com/sistemacar/parkapi/UsuarioIT.java
@@ -77,4 +77,22 @@ public class UsuarioIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 
+    @Test
+    public void createUsuario_ConUsernameRepetido_RetornarErrorMessageComStatus409(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/usuarios/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("angel@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+
+
+    }
+
 }
